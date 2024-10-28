5e34b2334e51fd7cd1f9dfa2c345286eec2d39bc
RubenPerezCastrillo <ruben.perez.castrillo@gmail.com>
2024-10-28 15:38:28 +0000

    Finished

--- src/js/component/Buttons.jsx
+++ src/js/component/Buttons.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 
-const Buttons = () => {
-    return(
-        <div className="d-flex align-items-center justify-content-center mt-5 gap-3">
-        <button>Alternar</button>
-        <button>Purpura</button>
+const Buttons = ({ isPaused, setIsPaused, setSelectedLight, isPurpleLightVisible, setIsPurpleLightVisible }) => {
+    return (
+        <div className="d-flex flex-column align-items-center justify-content-center mt-5 gap-3">
+            <button onClick={() => setIsPaused(!isPaused)}>
+                {isPaused ? 'Start lighting' : 'Stop lighting'}</button>
+            <button onClick={() => {
+                setSelectedLight('primary');
+                setIsPurpleLightVisible(!isPurpleLightVisible); 
+                if (isPurpleLightVisible) {
+                    alert("Blue light is eliminated to traffic light");
+                  } else {
+                    alert("Blue light is added to traffic light");
+                  }
+            }}>{isPurpleLightVisible ? 'Hide blue light' : 'Show blue light'}</button>
         </div>
     )
 }
 
 export default Buttons;
+{/* <button onClick={() => {
+                setSelectedLight('primary');
+                setIsPurpleLightVisible(true); // Mostrar la luz púrpura al hacer clic
+            }}>Purpura</button> */}
--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,27 +1,72 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import Buttons from "./Buttons";
 
 
 const TrafficLight = () => {
-    const [selectedLight, setSelectedLight] = useState(null);
+    const [selectedLight, setSelectedLight] = useState("danger");
+    const [isPaused, setIsPaused] = useState(false);
+    const intervalRef = useRef(null);
+    const [isPurpleLightVisible, setIsPurpleLightVisible] = useState(false);
+
+    useEffect(() => {
+        const startInterval = () => {
+            intervalRef.current = setInterval(() => {
+                setSelectedLight((prevLight) => {
+                    switch (prevLight) {
+                        case 'danger':
+                            return 'warning';
+                        case 'warning':
+                            return 'success';
+                        case 'success':
+                            return 'danger';
+                        default:
+                            return 'danger';
+                    }
+                });
+            }, 1000);
+        };
+
+        if (!isPaused) {
+            startInterval();
+        }
+
+        return () => clearInterval(intervalRef.current);
+    }, [isPaused]);
 
     const handleLightClick = (color) => {
         setSelectedLight(color);
+        clearInterval(intervalRef.current); // Detenemos el intervalo al hacer clic
+        intervalRef.current = null; // Reiniciamos la referencia
+        setIsPaused(true); // Marcamos como pausado
+        console.log("Color seleccionado:", color);
     };
 
     return (
         <div className="d-flex flex-column align-items-center justify-content-center">
             <div className="bg-black" style={{ height: '80px', width: '20px' }}>
             </div>
-            <div className="d-flex flex-column align-items-center justify-content-center bg-black rounded-1 gap-2" style={{ height: "250px", width: "100px" }}>
+            <div className="d-flex flex-column align-items-center justify-content-center bg-black gap-2" style={{ height: "250px", width: "100px" }}>
                 <div className={`bg-danger rounded-circle ${selectedLight === 'danger' ? 'selected' : ''}`} style={{ height: "70px", width: "70px" }} onClick={() => handleLightClick('danger')}></div>
                 <div className={`bg-warning rounded-circle ${selectedLight === 'warning' ? 'selected' : ''}`} style={{ height: "70px", width: "70px" }} onClick={() => handleLightClick('warning')}></div>
                 <div className={`bg-success rounded-circle ${selectedLight === 'success' ? 'selected' : ''}`} style={{ height: "70px", width: "70px" }} onClick={() => handleLightClick('success')}></div>
+
             </div>
-            {/* <div className="d-flex flex-column align-items-center justify-content-start bg-black rounded-1" style={{ height: "100px", width: "100px" }}>
-                <div className={`bg-primary rounded-circle ${selectedLight === 'primary' ? 'selected' : ''}`} style={{ height: "70px", width: "70px" }} onClick={() => handleLightClick('success')}>
+            {isPurpleLightVisible && (
+                <div className="d-flex flex-column align-items-center justify-content-start bg-black" style={{ height: "100px", width: "100px" }}>
+                    <div className={`bg-primary rounded-circle ${selectedLight === 'primary' ? 'selected' : ''}`} style={{ height: "70px", width: "70px" }} onClick={() => handleLightClick('primary')}>
+                    </div>
                 </div>
-            </div> */}
+            )}
+
+            <Buttons
+                isPaused={isPaused}
+                setIsPaused={setIsPaused}
+                setSelectedLight={setSelectedLight}
+                isPurpleLightVisible={isPurpleLightVisible}
+                setIsPurpleLightVisible={setIsPurpleLightVisible}
+            />
+
         </div>
     )
 }
